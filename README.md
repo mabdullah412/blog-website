69230d6a45a9d0d8202f142c860f642b12e58fd4
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-06-27 16:32:06 +0500

    add all ejs files, create homepage with starting content

--- README.md
+++ README.md
@@ -5,4 +5,12 @@ A simple blog website that lets the user to write and post blogs.
 
 # Tech
 
-HTML, CSS, NodeJS, EJS, MongoDB, REST API
+HTML, CSS, NodeJS, ExpressJS, EJS, MongoDB, REST API
+
+# Start or your computer
+
+First of all install the node_modules by typing npm install inside the project folder.
+
+blah
+blah
+blah
--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const homeStartingContent = 'Coruscant ben biggs watto yoda moff. Skywalker kessel cade gamorrean ackbar. Leia skywalker jawa utapau skywalker fett. Qui-gon biggs wicket grievous. Jango yoda binks skywalker dagobah dooku yoda. Jabba boba sidious lando. Solo wedge mon obi-wan skywalker. Fisto boba darth utapau darth wedge. Naboo greedo ben darth darth. Vader dooku obi-wan mothma boba amidala. Yavin mothma coruscant binks. Darth sebulba binks skywalker. Skywalker skywalker darth jade kamino sith moff moff fett. Biggs c-3po hoth antilles qui-gon moff.';
+const aboutContent = '';
+const contactContent = '';
 
 const app = express();
 const port = 3000;
 
 // set up express view engine view
 app.set('view engine', 'ejs');
+// use bodyparser
+app.use(bodyParser.urlencoded({extended: true}));
 // use static files
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('home', {homeStartingContent: homeStartingContent});
 });
 
 app.listen(port, function () {
