3f6acca089a942580f41c670a11f7ae0f82a9519
MAbdullahMughal <70143578+MAbdullahMughal@users.noreply.github.com>
2022-06-27 21:00:15 +0500

    complete project

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const lodash = require('lodash');
 
-const homeStartingContent = 'Coruscant ben biggs watto yoda moff. Skywalker kessel cade gamorrean ackbar. Leia skywalker jawa utapau skywalker fett. Qui-gon biggs wicket grievous. Jango yoda binks skywalker dagobah dooku yoda. Jabba boba sidious lando. Solo wedge mon obi-wan skywalker. Fisto boba darth utapau darth wedge. Naboo greedo ben darth darth. Vader dooku obi-wan mothma boba amidala. Yavin mothma coruscant binks. Darth sebulba binks skywalker. Skywalker skywalker darth jade kamino sith moff moff fett. Biggs c-3po hoth antilles qui-gon moff.';
-const aboutContent = '';
-const contactContent = '';
+const homeStartingContent = 'This is home Page. Coruscant ben biggs watto yoda moff. Skywalker kessel cade gamorrean ackbar. Leia skywalker jawa utapau skywalker fett. Qui-gon biggs wicket grievous. Jango yoda binks skywalker dagobah dooku yoda. Jabba boba sidious lando. Solo wedge mon obi-wan skywalker. Fisto boba darth utapau darth wedge. Naboo greedo ben darth darth. Vader dooku obi-wan mothma boba amidala. Yavin mothma coruscant binks. Darth sebulba binks skywalker. Skywalker skywalker darth jade kamino sith moff moff fett. Biggs c-3po hoth antilles qui-gon moff.';
+const aboutContent = 'This is about us Page. Coruscant ben biggs watto yoda moff. Skywalker kessel cade gamorrean ackbar. Leia skywalker jawa utapau skywalker fett. Qui-gon biggs wicket grievous. Jango yoda binks skywalker dagobah dooku yoda. Jabba boba sidious lando. Solo wedge mon obi-wan skywalker. Fisto boba darth utapau darth wedge. Naboo greedo ben darth darth. Vader dooku obi-wan mothma boba amidala. Yavin mothma coruscant binks. Darth sebulba binks skywalker. Skywalker skywalker darth jade kamino sith moff moff fett. Biggs c-3po hoth antilles qui-gon moff.';
+const contactContent = 'This is contact us Page. Coruscant ben biggs watto yoda moff. Skywalker kessel cade gamorrean ackbar. Leia skywalker jawa utapau skywalker fett. Qui-gon biggs wicket grievous. Jango yoda binks skywalker dagobah dooku yoda. Jabba boba sidious lando. Solo wedge mon obi-wan skywalker. Fisto boba darth utapau darth wedge. Naboo greedo ben darth darth. Vader dooku obi-wan mothma boba amidala. Yavin mothma coruscant binks. Darth sebulba binks skywalker. Skywalker skywalker darth jade kamino sith moff moff fett. Biggs c-3po hoth antilles qui-gon moff.';
 
 const app = express();
 const port = 3000;
@@ -16,8 +16,43 @@ app.use(bodyParser.urlencoded({extended: true}));
 // use static files
 app.use(express.static('public'));
 
+const posts = [];
+
 app.get('/', function (req, res) {
-    res.render('home', {homeStartingContent: homeStartingContent});
+    res.render('home', {homeStartingContent: homeStartingContent, posts: posts});
+});
+
+app.get('/about', function (req, res) {
+    res.render('about', {aboutContent: aboutContent});
+});
+
+app.get('/contact', function (req, res) {
+    res.render('contact', {contactContent: contactContent});
+});
+
+app.get('/compose', function (req, res) {
+    res.render('compose');
+});
+
+app.post('/compose', function (req, res) {
+    const post = {
+        title: req.body.postTitle,
+        body: req.body.postContent,
+    }
+    
+    posts.push(post);
+    res.redirect('/');
+});
+
+app.get('/posts/:title', function (req, res) {
+    const title = lodash.lowerCase(req.params.title);
+    
+    posts.forEach(function(post) {
+        if (post.title.toLowerCase() === title) {
+            // console.log('Match Found!');
+            res.render('post', {post: post});
+        }
+    });
 });
 
 app.listen(port, function () {
